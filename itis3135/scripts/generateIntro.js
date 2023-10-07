9475625792149be15252b072e18a472c9ffc6850
Jack Krueger <92689827+ItalianJack@users.noreply.github.com>
2023-10-07 19:22:01 -0400

    completed generate intro page

--- itis3135/scripts/generateIntro.js
+++ itis3135/scripts/generateIntro.js
@@ -18,7 +18,7 @@ function generateIntro() {
 
     let imageFigureElement = document.createElement("figure");
     let userImageElement = document.createElement("img");
-    userImageElement.setAttribute("src", "images/flightschool.jpg");
+    userImageElement.setAttribute("src", loadImage());
     imageFigureElement.appendChild(userImageElement);
     let figureCaption = document.getElementById("image-caption").value;
     let figureCaptionElement = document.createElement("figcaption");
@@ -27,31 +27,61 @@ function generateIntro() {
     main.appendChild(imageFigureElement);
 
     let mainListElement = document.createElement("ul");
-    mainListElement.appendChild(generateIntroTopic("background-personal", "Personal Background: "));
-    mainListElement.appendChild(generateIntroTopic("background-professional", "Professional Background: "));
-    mainListElement.appendChild(generateIntroTopic("background-academic", "Academic Background: "));
-    mainListElement.appendChild(generateIntroTopic("computer-platform", "Preferred Computing Platform: "));
+    mainListElement.appendChild(generateTopicListItem("background-personal", "Personal Background: "));
+    mainListElement.appendChild(generateTopicListItem("background-professional", "Professional Background: "));
+    mainListElement.appendChild(generateTopicListItem("background-academic", "Academic Background: "));
+    mainListElement.appendChild(generateTopicListItem("computer-platform", "Preferred Computing Platform: "));
+    mainListElement.appendChild(generateClassListItem());
+    mainListElement.appendChild(generateTopicListItem("memory-fact", "Fun Fact to remember me by: "));
+    mainListElement.appendChild(generateTopicListItem("also-share", "I'd also like to share: "));
+
     main.appendChild(mainListElement);
 
-    let classListElement = document.createElement("li");
-    let classListLabel = document.createElement("strong");
-    classListLabel.innerText = "Courses I'm Taking & Why: ";
-    classListElement.appendChild(classListLabel);
+    let reloadButton = document.createElement("button");
+    reloadButton.innerText = "New Intro";
+    reloadButton.setAttribute("onclick","location.reload()");
 
-    let classListInputs = document.getElementById("class-list").children;
-    for (let label in classListInputs) {
-        let input = label.getElementsByTagName("textarea")[0];
-        
-    }
+    main.appendChild(reloadButton);
 
     document.getElementById("main").replaceWith(main);
 }
 
-function generateIntroTopic(inputId, labelString) {
+function generateTopicListItem(inputId, labelString) {
     let listItemElement = document.createElement("li");
     let listItemLabel = document.createElement("strong");
     listItemLabel.innerText = labelString;
     listItemElement.appendChild(listItemLabel);
     listItemElement.innerHTML += document.getElementById(inputId).value;
     return listItemElement;
+}
+
+function generateClassListItem() {
+    let classListElement = document.createElement("li");
+    let classListLabel = document.createElement("strong");
+    classListLabel.innerText = "Courses I'm Taking & Why: ";
+    classListElement.appendChild(classListLabel);
+
+    let classListUlElement = document.createElement("ul");
+    let classListInputs = document.getElementById("class-list").children;
+    for (let i = 0; i < classListInputs.length; i++) {
+        // console.log(classListInputs[i].children[2].value);
+        let inputValue = classListInputs[i].children[2].value;
+        let item = document.createElement("li");
+        item.innerText = inputValue;
+        classListUlElement.appendChild(item);
+    }
+
+    classListElement.appendChild(classListUlElement);
+    return classListElement;
+}
+
+function loadImage() {
+    let image = document.getElementById("user-image").files[0];
+    if (image) {
+        let url = URL.createObjectURL(image);
+        console.log(url);
+        return url;
+    } else {
+        return "images/flightschool.jpg";
+    }
 }
