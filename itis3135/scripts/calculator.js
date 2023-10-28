78bd8952a715115e89b9360f700fe57c16bb7fd5
Jack Krueger <92689827+ItalianJack@users.noreply.github.com>
2023-10-28 15:15:11 -0400

    Calculator final

--- itis3135/scripts/calculator.js
+++ itis3135/scripts/calculator.js
@@ -3,22 +3,71 @@
 const buttonsDiv = document.querySelector("#calculator_buttons");
 const calcDisplay = document.querySelector("#calculator_display");
 
+let operation = null;
+let storedNum = 0;
+
 buttonsDiv.addEventListener("click", e => {
     let action = e.target.dataset.action;
-    if (!(action === undefined)) {
-        console.log(action);
-    } else {
-        console.log("number");
-        appendToCurrentNum(e.target.innerText);
-    }
-})
+    switch (action) {
+        default:
+            appendDisplayCurrent(e.target.innerText);
+            break;
+        case "clear":
+            calcDisplay.innerText = "0";
+            break;
+        case "add":
+        case "subtract":
+        case "multiply":
+        case "divide":
+            storeNumber();
+            operation = action;
+            console.log("Operation: " + operation);
+            break;
+        case "equals":
+            if (operation !== null) {
+                calcDisplay.innerText = calculate();
+                storedNum = 0;
+                operation = null;
+            }
+            break;
 
-function appendToCurrentNum(val) {
+    }
+});
+
+function appendDisplayCurrent(val) {
     if (calcDisplay.innerText === "0" && calcDisplay.innerText.length === 1) {
         calcDisplay.innerText = val;
+    } else {
+        calcDisplay.innerText += val;
     }
 }
 
 function clearCurrentNum() {
-    
+    calcDisplay.innerText = "0";
+}
+
+function storeNumber() {
+    if (storedNum !== 0) {
+        storedNum = calculate();
+    } else {
+        storedNum = parseFloat(calcDisplay.innerText);
+    }
+    clearCurrentNum();
+    console.log("Stored number: " + storedNum);
+}
+
+function calculate() {
+    switch (operation) {
+        case "add":
+            return storedNum + parseFloat(calcDisplay.innerText);
+        case "subtract":
+            return storedNum - parseFloat(calcDisplay.innerText);
+        case "divide":
+            return storedNum / parseFloat(calcDisplay.innerText);
+        case "multiply":
+            return storedNum * parseFloat(calcDisplay.innerText);
+        default:
+            console.log("Error in calculate()!");
+            return -1;
+    }
 }
