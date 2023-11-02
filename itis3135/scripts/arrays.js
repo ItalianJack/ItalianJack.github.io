fb739370c41c286f27c0e5cec1d149e3e2497596
Jack Krueger <92689827+ItalianJack@users.noreply.github.com>
2023-11-02 10:34:46 -0400

    arrays setup

---
+++ itis3135/scripts/arrays.js
@@ -0,0 +1,18 @@
+let persons = ["John Krueger", "Harry Potter", "Ronald Weasley", "Hermione Granger", "Neville Longbottom", "Fred Weasley", "Luna Lovegood"];
+let salaries = [12500, 7750, 6750, 8500, 9250, 5000, 6500];
+
+function addSalary() {
+
+}
+
+function displayResults() {
+
+}
+
+function displaySalary() {
+
+}
+
+document.querySelector("#addSalaryButton").addEventListener("click", addSalary);
+document.querySelector("#displayResultsButton").addEventListener("click", displayResults);
+document.querySelector("#displaySalaryButton").addEventListener("click", displaySalary);
