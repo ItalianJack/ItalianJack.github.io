a1f8e87a616bc677f4bdb81e141500850dfb04b0
Jack Krueger <92689827+ItalianJack@users.noreply.github.com>
2023-11-04 13:37:21 -0400

    top menu complete for ELP

---
+++ itis3135/emmalynne.photography/scripts/toggleNav.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+    let x = document.querySelector("nav");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
